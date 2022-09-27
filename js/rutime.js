09e8b07b43bfdf236c66a2c56dfd1187b04cb640
陈志勇0 <2017721025@qq.com>
2022-09-27 14:10:05 +0800

    Site updated: 2022-09-27 14:10:04

--- js/rutime.js
+++ js/rutime.js
@@ -1,5 +1,5 @@
 setInterval(() => {
-    let create_time = Math.round(new Date('2019-04-17 00:00:00').getTime() / 1000); //在此行修改建站时间
+    let create_time = Math.round(new Date('2021-04-17 00:00:00').getTime() / 1000); //在此行修改建站时间
     let timestamp = Math.round((new Date().getTime()) / 1000);
     let second = timestamp - create_time;
     let time = new Array(0, 0, 0, 0, 0);
@@ -27,10 +27,10 @@ setInterval(() => {
       time[4] = nol(second);
     }
     if ((Number(time[2])<22) && (Number(time[2])>7)){
-      currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/CHEN屋-营业中-6adea8?style=social&logo=cakephp' title='距离你还差一个IKUN~'><div id='runtime'>" + time[0] + ' 年 ' + time[1] + ' 日 ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+      currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/蛋（CHEN）糕-营业中-6adea8?style=social&logo=cakephp' title='距离你还差一个IKUN~'><div id='runtime'>" + time[0] + ' 年 ' + time[1] + ' 日 ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
     }
     else{
-      currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/CHEN屋-打烊了-6adea8?style=social&logo=coffeescript' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' 年 ' + time[1] + ' 日 ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+      currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/蛋（CHEN）糕-打烊了-6adea8?style=social&logo=coffeescript' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' 年 ' + time[1] + ' 日 ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
     }
     document.getElementById("workboard").innerHTML = currentTimeHtml;
   }, 1000);
